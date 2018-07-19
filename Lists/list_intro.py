1f1891ddab550c539b7da650198cabb559e85929
dsbrown1331 <dsbrown1331@gmail.com>
2018-07-19 13:27:51 -0500

    stuff for class

---
+++ Lists/list_intro.py
@@ -0,0 +1,17 @@
+#this is how you make an empty list
+empty_list = []
+
+#here is list with three integer elements
+three_nums = [1,2,3]
+
+#we can access elements in the list using []
+print("Printing out items in list:")
+print(three_nums[0])
+print(three_nums[1])
+print(three_nums[2])
+
+#we can have lists of strings
+days_of_week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+#we can print an entire list
+print(days_of_week)
+print("The third day of the week is ", days_of_week[2])
